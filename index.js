73079daccb61e656a6e1aa2e9cde381e10e90c12
muliamaulana <halo@muliamaulana.com>
2018-12-01 09:20:10 +0700

    modified index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/** @format */
+// Import a library to help create component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create a component
+const App = () => (
+    <Text>Some Text</Text>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+// Render it to device
+AppRegistry.registerComponent('HelloReactNative', () => App);
