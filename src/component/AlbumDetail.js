a48437a92afb5cfc9bd74265220dd14f1c7bb7b7
muliamaulana <halo@muliamaulana.com>
2018-12-10 17:28:58 +0700

    Add button and styling button

--- src/component/AlbumDetail.js
+++ src/component/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import CardView from './CardView';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image, image } = album;
@@ -24,6 +25,10 @@ const AlbumDetail = ({ album }) => {
 
             <CardSection>
                 <Image style={imageStyle} source={{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button />
             </CardSection>
         </CardView>
     );
